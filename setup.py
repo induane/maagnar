a8c14d0510bafb6b2ce9351d95288f97ca0f23e9
Brant Watson <oldspiceap@gmail.com>
2021-02-11 12:07:30 -0600

    Add long description

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="maagnar",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/induane/maagnar",
     author="Brant Watson",
     author_email="oldspiceap@gmail.com",
     description="Library and command line entry point for generating anagrams",
+    long_description="Library and command line entry point for generating anagrams",
     license="MIT License",
     packages=find_packages("src"),
     package_dir={"": "src"},
